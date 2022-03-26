688924e113eed469ccafaa8be6279de19f95d620
muxing <linsichen2018@gmail.com>
2022-03-25 22:30:40 -0400

    day01

---
+++ DynamicProgramming/ZeroOneknapsack.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+
+using namespace std;
+
+const int N = 1010;
+
+
+// n is the amount of items
+int n, capacity;
+int dp[N][N];
+int volume[N], weight[N];
+int dp2[N];
+
+int solution1() {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j <= capacity; j++) {
+			dp[i][j] = dp[i - 1][j];
+			if (j - volume[i] >= 0)
+				dp[i][j] = max(dp[i][j], dp[i - 1][j - volume[i]] + weight[i]);
+		}
+	}
+	int res = 0;
+	for (int i = 0; i <= capacity; i++) res = max(res, dp[n][i]);
+	cout << res << endl;
+	return 0;
+}
+
+int solution2() {
+	for (int i = 1; i <= n; i++) {
+		for (int j = capacity; j >= volume[i]; j--) {
+			dp2[j] = max(dp2[j], dp2[j - volume[i]] + weight[i]);
+		}
+	}
+	cout << dp2[capacity] << endl;
+	This is a beginner's repository, I'll start with the basics
+
+
+	return 0;
+}
+
+
+
+int main() {
+	cin >> n >> capacity;
+	for (int i = 1; i <= n; i++) cin >> volume[i] >> weight[i];
+	solution2();
+	return 0;
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Acwing_Algorithm_Basic
+
+## Hello World
+
+I am an algorithm competition enthusiast, this repository will record my algorithm competition and c++ journey
+
+This is a beginner's repository, I'll start with the basics
+
+I try to write understandable code, but if I follow the fast mode of writing algorithm (every variable is as short as possible), I will leave enough comments at the beginning
+
+Current working on basic dynamic programming and graph theory
+
+Thanks to SublimeAStyleFormatter
